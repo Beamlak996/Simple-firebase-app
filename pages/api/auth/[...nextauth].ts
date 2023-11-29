92141bf4eb7dd4b0f6f2b87b70d57035e44c994b
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-11-29 01:05:14 -0900

    Next-Auth complete

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,36 @@
+import { auth } from "@/firebase/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {},
+      async authorize(credentials, req): Promise<any> {
+        return await signInWithEmailAndPassword(
+          auth,
+          (credentials as any).email || "",
+          (credentials as any).password || ""
+        )
+          .then((userCredential) => {
+            if (userCredential.user) {
+              return userCredential.user;
+            }
+            return null;
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(error);
+          });
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/signin"
+  }
+};
+
+export default NextAuth(authOptions)
